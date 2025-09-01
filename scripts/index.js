1aa840f8b5879a3ae0cf40964ecccbb61a1f2656
Cennac <139062520+CennacEh@users.noreply.github.com>
2025-09-01 19:08:30 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 import { world, system } from "@minecraft/server";
 
-system.runInterval(function() {
-    world.sendMessage("Hello, World!")
-}, 20)
+system.runInterval(() => {
+    world.sendMessage("Hello, World!");
+}, 20);
 
 
