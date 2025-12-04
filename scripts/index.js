7a523371e6ca153e6e9194cd9cc658058a450c70
Cennac <139062520+CennacEh@users.noreply.github.com>
2025-12-04 16:16:12 +0100

    added useless comments that makes stuff even more complicated

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
+//Import world and system class
 import { world, system } from "@minecraft/server";
 
+//runInterval re-runs code inside callback till you stop it
 system.runInterval(() => {
-    world.sendMessage("Hello, World!");
-}, 20);
+    world.sendMessage("Hello, World!"); //Send Hello world! in game chat
+}, 20); //the 20 tells it to run every 1 second (20 ticks = 1 sec)
 
 
